0acb8ba322816005084daa9eb1dd4acf2acc0f0f
kathsup <katherinencarvallo22@gmail.com>
2025-02-06 20:03:52 -0600

    mieo

--- src/compartir/Compartir.java
+++ src/compartir/Compartir.java
@@ -5,6 +5,7 @@ public class Compartir {
     public static void main(String[] args) {
         System.out.println("prueba");
         System.out.println("que estres");
+        //hay que cambiarse a merca 
     }
     
 }
